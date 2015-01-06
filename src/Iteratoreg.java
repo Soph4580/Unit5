e62f6573ea6d23a883526e8f7de017c2aa3c95cf
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2015-01-06 09:18:30 -0500

    starting unit 5

---
+++ src/Iteratoreg.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Iteratoreg {
+static ArrayList list = new ArrayList();
+static ListIterator li;
+    
+    public static void main(String[] args) {
+       list.add("a");
+       list.add("b");
+       list.add("c");
+       list= list.ListIterator();
+    }
+    
+}
